aad8ba6c4af435b63c12b47967a1aa1e0bbdb0c1
thipham2018 <trangphamtmp@gmail.com>
2023-08-15 11:41:22 -0400

    push1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Nav1 from './components/nav1'
+
+function App() {
+
+  return (
+    <>
+    <Nav1 />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav1.jsx
@@ -0,0 +1,16 @@
+const Nav1 = () => {
+  return ( 
+    <div className="flex justify-between">
+      <div>
+        <header>
+          trang
+        </header>
+        <main>
+          home
+        </main>
+      </div>
+    </div>
+   );
+}
+ 
+export default Nav1;
